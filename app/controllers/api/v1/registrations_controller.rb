527bafe898360811ec94ea655aace29008b80e62
shankar <shankar.yuvasoft15@gmail.com>
2017-05-30 18:10:52 +0530

    Signup via API

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::RegistrationsController < Api::V1::ApiController
+  skip_before_action :authenticate, :only => [:create ]
+  protect_from_forgery with: :null_session
+
+  def create
+    user = User.new(params[:user].permit!)
+    if user.save
+      render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json=> user.errors, :status=>422
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'registrations/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       get 'sessions/create'
     end
   end
